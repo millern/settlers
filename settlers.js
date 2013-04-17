564547fbf242c5bca789ddc6cb082b4f46567863
Tucker Joseph <rtjoseph11@gmail.com>
2013-04-16 17:41:16 -0700

    updated gitignore and settlers

--- settlers.js
+++ settlers.js
@@ -1,19 +1,20 @@
+GameBoards = new Meteor.Collection('gameboards');
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to settlers.";
+    return 'Welcome to Settlers.';
   };
-
   Template.hello.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log(GameBoards.findOne({name: 'board 1'}));
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    GameBoards.insert({name: 'board 1'});
     // code to run on server at startup
     
   });
